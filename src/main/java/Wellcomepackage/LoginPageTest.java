d3d2958e4721f9530005c9cc1afc223ff9c4a71b
itssantoshyadav <itssantoshyadav@outlook.com>
2023-08-15 12:21:54 +0530

    Initial Project exporting from eclipe

---
+++ src/main/java/Wellcomepackage/LoginPageTest.java
@@ -0,0 +1,36 @@
+package Wellcomepackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	WebDriver driver = new EdgeDriver();
+	
+	@BeforeSuite
+	public void beforesuittesting() {
+
+		driver.get("https://the-internet.herokuapp.com/login");
+		System.out.println("BeforeSuit");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	}
+		@Test
+	public void loginPageaTest() {
+		driver.findElement(By.name("username")).sendKeys("tomsmith");
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		//driver.findElement(By.className("fa fa-2x fa-sign-in")).click();
+		
+	}
+		
+		@AfterSuite
+		public void close() {
+			driver.close();
+		}
+
+}
